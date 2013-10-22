40da26fde5eb580fe708d5031312f4f0576de29d
Wouter Mooij <wouter@wercker.com>
2013-10-22 17:43:58 +0200

    Build report

--- run.sh
+++ run.sh
@@ -5,4 +5,12 @@ else
   echo "Building with default configuration."
 fi
 
-/cygdrive/c/Windows/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe $MSBUILD_CONFIG "$WERCKER_MSBUILD_SOLUTION"
+WERCKER_STEP_ROOT_WIN='c:\cygwin'$(echo $WERCKER_STEP_ROOT | sed 's/\//\\/g')
+WERCKER_REPORT_ARTIFACTS_DIR_WIN='c:\cygwin'$(echo $WERCKER_REPORT_ARTIFACTS_DIR | sed 's/\//\\/g')
+LOGGER_DLL=$WERCKER_STEP_ROOT_WIN'\ThoughtWorks.CruiseControl.MSBuild.dll'
+
+LOGGER_CONFIG='/logger:'$LOGGER_DLL';'$WERCKER_REPORT_ARTIFACTS_DIR_WIN'buildresult.xml'
+
+echo "/cygdrive/c/Windows/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe $MSBUILD_CONFIG /clp:showcommandline $LOGGER_CONFIG \"$WERCKER_MSBUILD_SOLUTION\""
+
+/cygdrive/c/Windows/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe $MSBUILD_CONFIG /clp:showcommandline $LOGGER_CONFIG "$WERCKER_MSBUILD_SOLUTION"
